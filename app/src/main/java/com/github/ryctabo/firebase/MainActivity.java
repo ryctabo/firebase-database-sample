529a7c8e5e471996969f4c6dc1d5d1794425a348
Gustavo Pacheco <ryctabo@gmail.com>
2018-02-06 19:00:40 -0500

    Initial Commit

---
+++ app/src/main/java/com/github/ryctabo/firebase/MainActivity.java
@@ -0,0 +1,17 @@
+package com.github.ryctabo.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Gustavo Pacheco (ryctabo at gmail.com)
+ * @version 1.0-SNAPSHOT
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
